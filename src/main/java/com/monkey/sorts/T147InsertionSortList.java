d873ad639a96433c1884cab4d06e0843cb499f56
txu1 <txu1@i9i8.com>
2021-01-28 20:23:42 +0800

    bug fix

--- src/main/java/com/monkey/sorts/T147InsertionSortList.java
+++ src/main/java/com/monkey/sorts/T147InsertionSortList.java
@@ -35,7 +35,7 @@ public class T147InsertionSortList {
         ListNode curr = head.next;
 
         while (curr != null) {
-            if (lastSorted.val < curr.val) {
+            if (lastSorted.val <= curr.val) {
                 lastSorted = lastSorted.next;
             } else {
                 ListNode prev = dummyNode;
@@ -52,13 +52,14 @@ public class T147InsertionSortList {
     }
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(4);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(1);
-        ListNode node4 = new ListNode(3);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        //ListNode node3 = new ListNode(1);
+        //ListNode node4 = new ListNode(3);
         node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
+        node2.next = null;
+        //node2.next = node3;
+        //node3.next = node4;
 
         T147InsertionSortList t147InsertionSortList = new T147InsertionSortList();
         ListNode sortedNode = t147InsertionSortList.insertionSortList(node1);
